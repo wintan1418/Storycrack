5528dd6f9dc23b2b4e2942520fe8dd62368803bc
wintan1418 <wintan1418@gmail.com>
2023-07-10 18:13:13 +0100

    Fix issues

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import openai
 import time
 from flask import Flask, render_template, request
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 
 app = Flask(__name__)
