99e0d94ed020da2c8b1320b1e91ab3b2de0c6e7e
wintan1418 <wintan1418@gmail.com>
2023-07-11 15:20:23 +0100

    Fix error

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import os
 import openai
 import time
 from flask import Flask, render_template, request
 
-openai.api_key = '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'
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 app = Flask(__name__)
 
